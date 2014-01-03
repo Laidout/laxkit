b91c473eb45acd91eda6f1f8cf9c824bd3e944bb
Tom Lechner <tom@tomlechner.com>
2014-01-03 17:39:36 +0100

    Rehabilitated freehand interface to run. Needs smoothing work.

--- lax/interfaces/freehandinterface.cc
+++ lax/interfaces/freehandinterface.cc
@@ -18,15 +18,18 @@
 //    License along with this library; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-//    Copyright (C) 2004-2006,2011 by Tom Lechner
+//    Copyright (C) 2004-2006,2011,2014 by Tom Lechner
 //
 
 
 
-#include <lax/interfaces/somedatafactory.h>
 #include <lax/interfaces/freehandinterface.h>
-#include <lax/interfaces/bezinterface.h>
-#include <lax/interfaces/somedata.h>
+
+#include <lax/interfaces/somedatafactory.h>
+#include <lax/interfaces/pathinterface.h>
+#include <lax/language.h>
+
+#include <lax/lists.cc>
 
 using namespace Laxkit;
 
@@ -38,96 +41,75 @@ using namespace std;
 
 namespace LaxInterfaces {
 
-//----- BezData listed here only as reference, it is defined in bezinterface.cc:
-//#define BEZS_CLOSED	1
-//#define BEZS_STIFF_EQUAL	0
-//#define BEZS_STIFF_NEQUAL	1
-//#define BEZS_NSTIFF_EQUAL	2
-//#define BEZS_NSTIFF_NEQUAL	3
-//
-//class BezData : public anObject
-//{
-//  public:
-//	int style;
-//	int npoints; // is all points, not just vertices
-//	flatpoint *points;
-//	int *pointstyles; // length is n/3+1
-//	BezData();
-//	BezData(int cstyle);
-//	~BezData();
-//	int AddPoint(int where,flatpoint c1,flatpoint v,flatpoint c2,int pointstyle);
-//	int DeletePoint(int index);
-//	const char *whattype() { return "BezData"; }
-//};
 
 //----------------------------------------------------------------
 
 /*! \class FreehandInterface
  * \ingroup interfaces
- * \brief Interface to create bezier lines from freehand motion.
+ * \brief Interface to create bezier or straight lines from freehand motion.
  *
  *   On mouse down, this records all mouse movement, and converts
- *   the points into a bez curve (linear/quad/cubic) on mouse up.
+ *   the points into a bez curve (linear or cubic) on mouse up.
  *
  * \todo ***Currently, this sucks. Should figure out how Sodipodi/inkscape does it.
  *  
- * \todo ***should be able to add to a current BezData??
- *   ***make choice beteen make LinesData or BezData (or Path?)
- *   *** make closed when final point==first point
+ * \todo *** make closed when final point is close to first point
  */
 
 
-FreehandInterface::FreehandInterface(int nid,Displayer *ndp) : anInterface(nid,ndp)
+FreehandInterface::FreehandInterface(anInterface *nowner, int nid, Displayer *ndp)
+ : anInterface(nowner,nid,ndp)
 {
-	if (app) deconcolor=app->rgbcolor(128,128,128);
-	else deconcolor=38066;
-	decoffcolor=~0;
+	freehand_style=0;
+
+	linecolor .rgbf(0,0,.5);
+	pointcolor.rgbf(.5,.5,.5);
+
 	linestyle.color.red  =linestyle.color.alpha=0xffff;
 	linestyle.color.green=linestyle.color.blue =0;
 
-	data=NULL;
 	mindist=40;
-	
+
 	showdecs=1;
-	mask=ButtonPressMask|ButtonReleaseMask|PointerMotionMask|KeyPressMask|KeyReleaseMask;
-	buttonmask=Button1Mask;
-	buttondown=0;
 	needtodraw=1;
 }
 
 FreehandInterface::~FreehandInterface()
 {
-	if (data) data->dec_count();
-	data=NULL;
 }
+
+const char *FreehandInterface::Name()
+{ return _("Freehand Lines"); }
+
 
 //! Return new FreehandInterface.
 /*! If dup!=NULL and it cannot be cast to ImageInterface, then return NULL.
  */
 anInterface *FreehandInterface::duplicate(anInterface *dup)
 {
-	if (dup==NULL) dup=new FreehandInterface(id,NULL);
+	if (dup==NULL) dup=new FreehandInterface(NULL,id,NULL);
 	else if (!dynamic_cast<FreehandInterface *>(dup)) return NULL;
 	return anInterface::duplicate(dup);
 }
 
 int FreehandInterface::UseThis(anObject *nobj, unsigned int mask)
-{
-	if (!nobj) return 1;
-	LineStyle *ls;
-	if (ls=dynamic_cast<LineStyle *>(nobj), ls!=NULL) {
-		if (mask&GCForeground) { 
-			if (data) data->linestyle.color=ls->color;
-			else linestyle.color=ls->color;
-		}
-		if (mask&GCLineWidth) {
-			if (data) data->linestyle.width=ls->width;
-			else linestyle.width=ls->width;
-		}
-		needtodraw=1;
-		return 1;
-	}
-	return 0;
+{ // ***
+	return 1;
+//	if (!nobj) return 1;
+//	LineStyle *ls;
+//	if (ls=dynamic_cast<LineStyle *>(nobj), ls!=NULL) {
+//		if (mask&GCForeground) { 
+//			if (data) data->linestyle.color=ls->color;
+//			else linestyle.color=ls->color;
+//		}
+//		if (mask&GCLineWidth) {
+//			if (data) data->linestyle.width=ls->width;
+//			else linestyle.width=ls->width;
+//		}
+//		needtodraw=1;
+//		return 1;
+//	}
+//	return 0;
 }
 
 int FreehandInterface::InterfaceOn()
@@ -139,9 +121,16 @@ int FreehandInterface::InterfaceOn()
 
 int FreehandInterface::InterfaceOff()
 { 
+	Clear(NULL);
 	showdecs=0;
 	needtodraw=1;
 	return 0;
+}
+
+void FreehandInterface::Clear(SomeData *d)
+{
+	lines.flush();
+	deviceids.flush();
 }
 
 /*! \todo *** this is really cheezy, as it just copies bez->Refresh()
@@ -150,112 +139,144 @@ int FreehandInterface::Refresh()
 { 
 	//DBG cerr <<"  Freehand trying to startdrawing"<<endl;
 
-	if (!data || !data->npoints) {
-		if (needtodraw) needtodraw=0;
-		return 0;
-	}
+	if (needtodraw==0) return 0;
+	needtodraw=0;
 
-	if (data->npoints) {
-	//DBG cerr <<" Freehand: Drawing..";
+	if (lines.n==0) return 0;
+	
+	dp->LineAttributes(1,LineSolid,LAXCAP_Round,LAXJOIN_Round);
+
+	RawPointLine *line;
+	for (int c=0; c<lines.n; c++) {
+		line=lines.e[c];
+		if (line->n==0) continue;
 
 		 // draw curve
-		dp->NewFG(&data->linestyle.color);
-		dp->LineAttributes(data->linestyle.width,LineSolid,data->linestyle.capstyle,data->linestyle.joinstyle);
-		if (data->npoints>3) dp->drawbez(data->points,data->npoints/3,data->style&BEZS_CLOSED,50,1,1); // drawcurve
-		dp->LineAttributes(0,LineSolid,data->linestyle.capstyle,data->linestyle.joinstyle);
-		int c;
+		dp->NewFG(&linecolor);
+		for (int c2=0; c2<line->n; c2++) {
+			if (c2==0) dp->moveto(line->e[c2]->p);
+			else dp->lineto(line->e[c2]->p);
+		}
+		dp->stroke(0);
 
 		 // draw control points
 		if (showdecs) {
-			dp->NewFG(deconcolor);
+			dp->NewFG(&pointcolor);
 			 // draw little circles
-			for (c=0; c<data->npoints; c++) {
-				flatpoint p=realtoscreen(data->points[c]);
-				int x=(int)p.x,y=(int)p.y;
-				if (c%3!=1) dp->draw(x,y,3);
-				else {
-					switch (data->pointstyles[c/3]) {
-						case BEZS_STIFF_EQUAL: dp->draw(x,y,5,5,0,0); break; // circle
-						case BEZS_NSTIFF_EQUAL: dp->draw(x,y,5,5,0,3); break; // tri up
-						case BEZS_STIFF_NEQUAL: dp->draw(x,y,5,5,0,1); break; // square
-						case BEZS_NSTIFF_NEQUAL: dp->draw(x,y,5,5,0,2); break; // diamond
-					}
-				}
-			}
-			 // draw control rods
-			for (c=0; c<data->npoints; c+=3) {
-				if (c+1<data->npoints) dp->drawrline(data->points[c],data->points[c+1]);
-				if (c+2<data->npoints) dp->drawrline(data->points[c+1],data->points[c+2]);
+			for (int c2=0; c2<line->n; c2++) {
+				dp->drawpoint(line->e[c2]->p,3,1);
 			}
 		}
 	}
 
 	//DBG cerr <<"   Freehand all done drawing.\n";
-	needtodraw=0;
 	return 0;
 }
 
+int FreehandInterface::findLine(int id)
+{
+	for (int c=0; c<deviceids.n; c++) if (deviceids[c]==id) return c;
+	return -1;
+}
+
 //! Start a new freehand line.
-int FreehandInterface::LBDown(int x,int y,unsigned int state,int count) 
+int FreehandInterface::LBDown(int x,int y,unsigned int state,int count, const Laxkit::LaxMouse *d) 
 {
 	DBG cerr <<"freehandlbd:"<<x<<','<<y<<"..";
-	buttondown|=LEFTBUTTON;
-	mx=x;
-	my=y;
+	buttondown.down(d->id,LEFTBUTTON,x,y);
 
-	data=NULL;
-	if (somedatafactory) {
-		data=static_cast<BezData *>(somedatafactory->newObject(LAX_BEZDATA));
-	} 
-	if (!data) data=new BezData;
-	
-	data->linestyle=linestyle;
+	int i=findLine(d->id);
+	if (i>=0) {
+		lines.remove(i);
+		deviceids.remove(i);
+	}
+
+	RawPointLine *line=new RawPointLine;
 	flatpoint p=dp->screentoreal(x,y);
-	data->AddPoint(-1,p,p,p,BEZS_STIFF_NEQUAL);
+	line->push(new RawPoint(p));
+	lines.push(line);
+	deviceids.push(d->id);
 	
 	DBG cerr <<"../freehandiLbd\n";
 	return 0;
 }
 
+int FreehandInterface::send(int i)
+{
+	if (i<0 || i>=lines.n) return 1;
+
+	RawPointLine *line=lines.e[i];
+	
+	PathsData *paths=new PathsData;
+	for (int c=0; c<line->n; c++) {
+		paths->append(line->e[c]->p);
+	}
+
+	if (owner) {
+		RefCountedEventData *data=new RefCountedEventData(paths);
+		app->SendMessage(data,owner->object_id,"FreehandInterface", object_id);
+
+	} else {
+		if (viewport) viewport->NewData(paths,NULL);
+	}
+
+	paths->dec_count();
+	
+	return 0;
+}
+
 //! Finish a new freehand line by calling newData with it.
-int FreehandInterface::LBUp(int x,int y,unsigned int state) 
+int FreehandInterface::LBUp(int x,int y,unsigned int state, const Laxkit::LaxMouse *d) 
 {
 	DBG cerr <<"  freehandLbup..";
-	if (!buttondown) return 0;
-	buttondown&=(~LEFTBUTTON);
 
-	 // add the bezcurve to datastack
-	 // ***should check for closed path???
-	if (viewport) viewport->NewData(data);//count had 1 from LBDown, this incs it by another 1
-	data->dec_count();
-	data=NULL;
+	if (!buttondown.isdown(d->id,LEFTBUTTON)) return 1;
+	int dragged=buttondown.up(d->id,LEFTBUTTON);
 
+	int i=findLine(d->id);
+	if (i<0) return 0; //line missing! do nothing
+
+	DBG cerr <<"  *** FreehandInterface should check for closed path???"<<endl;
+
+	if (!dragged && lines.e[i]->n>1) {
+		send(i);
+	}
+
+	deviceids.remove(i);
+	lines.remove(i);
+
+	needtodraw=1;
 	return 0;
-	DBG cerr <<"./bezLbup ";
 }
 
 /*! \todo *** this isn't very sophisticated, for elegance, should use some kind of 
  * bez curve fitting to cut down on unnecessary points should use a timer so 
  * stopping makes sharp corners and closer spaced points?
  */
-int FreehandInterface::MouseMove(int x,int y,unsigned int state) 
+int FreehandInterface::MouseMove(int x,int y,unsigned int state, const Laxkit::LaxMouse *d)
 {
-	if (!(buttondown==LEFTBUTTON)) return 1;
-	//DBG cerr <<" buttondown="<<buttondown<<"  ";
+	if (!buttondown.any()) return 1;
 
-	if ((x-mx)*(x-mx)+(y-my)*(y-my)<mindist*mindist) return 1;
+	int i=findLine(d->id);
+	if (i<0) return 0;
+
+	int mx=0,my=0;
+	buttondown.move(d->id, x,y, &mx,&my);
+
+	//if ((x-mx)*(x-mx)+(y-my)*(y-my)<mindist*mindist) return 1;
 	flatpoint p=dp->screentoreal(x,y);
-	data->AddPoint(-1,p,p,p,BEZS_STIFF_NEQUAL);
-	if (data->npoints>2*3) {
-		int n=data->npoints;
-		p=data->points[n-2]-data->points[n-8];
-		data->points[n-6]+=((data->points[n-8]-data->points[n-5])||p)/2;
-		data->points[n-4]+=((data->points[n-2]-data->points[n-5])||p)/2;
-	}
+	RawPointLine *line=lines.e[i];
+	line->push(new RawPoint(p)); // *** need to extract pressure and time
+
+//	if (data->npoints>2*3) {
+//		int n=data->npoints;
+//		p=data->points[n-2]-data->points[n-8];
+//		data->points[n-6]+=((data->points[n-8]-data->points[n-5])||p)/2;
+//		data->points[n-4]+=((data->points[n-2]-data->points[n-5])||p)/2;
+//	}
+
 	needtodraw=1;
-	my=y;
-	mx=x;
-	return 1;
+	return 0;
 }
 
 } // namespace LaxInterfaces
--- lax/interfaces/freehandinterface.h
+++ lax/interfaces/freehandinterface.h
@@ -18,16 +18,36 @@
 //    License along with this library; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-//    Copyright (C) 2004-2007,2011 by Tom Lechner
+//    Copyright (C) 2004-2007,2011,2014 by Tom Lechner
 //
 #ifndef _LAX_FREEHANDINTERFACE_H
 #define _LAX_FREEHANDINTERFACE_H
 
 #include <lax/interfaces/aninterface.h>
-#include <lax/interfaces/bezinterface.h>
 #include <lax/interfaces/linestyle.h>
 
 namespace LaxInterfaces { 
+
+
+enum FreehandEditorStyles {
+	FREEHAND_Till_Closed =(1<<0), //mouse down drag out a line, up and clicking adds points
+	FREEHAND_Coordinates =(1<<1), //Construct Coordinate points
+	FREEHAND_Flatpoints  =(1<<2), //Create a list of flatpoints
+	FREEHAND_Path        =(1<<3),      //Create a PathsData
+	FREEHAND_ScrollWindow=(1<<4), //scroll viewport if off screen
+	FREEHAND_MAX
+};
+
+class RawPoint {
+  public:
+	flatpoint p;
+	clock_t time;
+	double pressure;
+	double tiltx,tilty;
+	RawPoint(flatpoint pp) { p=pp; time=0; pressure=0; tiltx=tilty=0; }
+};
+
+typedef Laxkit::PtrStack<RawPoint> RawPointLine;
 
 class FreehandInterface : public anInterface
 {
@@ -35,25 +55,43 @@ class FreehandInterface : public anInterface
 	char showdecs;
 	int mx,my;
 	LineStyle linestyle;
+	//Laxkit::ShortcutHandler *sc;
+
+	int findLine(int id);
+
+	virtual int send(int i);
+
   public:
-  int mindist;
-	unsigned long deconcolor,decoffcolor;
-	BezData *data;
-	FreehandInterface(int nid,Laxkit::Displayer *ndp);
-	~FreehandInterface();
+	unsigned int freehand_style;
+	double mindist;
+	Laxkit::ScreenColor linecolor;
+	Laxkit::ScreenColor pointcolor;
+
+	Laxkit::PtrStack<RawPointLine> lines;
+	Laxkit::NumStack<int> deviceids;
+
+	FreehandInterface(anInterface *nowner, int nid,Laxkit::Displayer *ndp);
+	virtual ~FreehandInterface();
 	virtual anInterface *duplicate(anInterface *dup);
-	virtual int LBDown(int x,int y,unsigned int state,int count);
-	virtual int LBUp(int x,int y,unsigned int state);
-	virtual int MouseMove(int x,int y,unsigned int state);
-//	virtual int CharInput(unsigned int ch, const char *buffer,int len,unsigned int state);
-//	virtual int CharRelease(unsigned int ch,unsigned int state);
+	const char *Name();
+	const char *whattype() { return "FreehandInterface"; }
+	const char *whatdatatype() { return NULL; } // is creation only
+	//virtual Laxkit::ShortcutHandler *GetShortcuts();
+	//virtual int PerformAction(int actionnumber);
+
 	virtual int UseThis(Laxkit::anObject *nlinestyle,unsigned int mask=0);
 	virtual int InterfaceOn();
 	virtual int InterfaceOff();
-	virtual void Clear(SomeData *d) { if ((!d && data) || (d && d==data)) { data->dec_count(); data=NULL; } }
-	const char *whattype() { return "FreehandInterface"; }
-	const char *whatdatatype() { return "none"; } // is creation only
-	int Refresh();
+	virtual void Clear(SomeData *d);
+	virtual int Refresh();
+	virtual int MouseMove(int x,int y,unsigned int state, const Laxkit::LaxMouse *d);
+	virtual int LBDown(int x,int y,unsigned int state,int count, const Laxkit::LaxMouse *d);
+	virtual int LBUp(int x,int y,unsigned int state, const Laxkit::LaxMouse *d);
+	//virtual int WheelUp  (int x,int y,unsigned int state,int count, const Laxkit::LaxMouse *d);
+	//virtual int WheelDown(int x,int y,unsigned int state,int count, const Laxkit::LaxMouse *d);
+	//virtual int CharInput(unsigned int ch, const char *buffer,int len,unsigned int state, const Laxkit::LaxKeyboard *d);
+	//virtual int KeyUp(unsigned int ch,unsigned int state, const Laxkit::LaxKeyboard *d);
+
 };
 
 } // namespace LaxInterfaces
