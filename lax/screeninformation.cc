c5a141e5c6bc7c6f53a5e252f8057b432df9eb3e
Tom Lechner <tom@tomlechner.com>
2018-12-02 11:56:18 -0800

    Put ScreenInformation class in own file, independent of anXApp

---
+++ lax/screeninformation.cc
@@ -0,0 +1,48 @@
+//
+//	
+//    The Laxkit, a windowing toolkit
+//    Please consult https://github.com/Laidout/laxkit about where to send any
+//    correspondence about this software.
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Library General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Library General Public License for more details.
+//
+//    You should have received a copy of the GNU Library General Public
+//    License along with this library; If not, see <http://www.gnu.org/licenses/>.
+//
+//    Copyright (C) 2004-2013 by Tom Lechner
+//
+
+#include <lax/screeninformation.h>
+#include <lax/strmanip.h>
+
+
+
+namespace Laxkit {
+
+
+//-------------------------- ScreenInformation ----------------------------------------
+/*! \class ScreenInformation
+ * \brief Information about screens and monitors.
+ */
+
+/*! Return monitor matching this name.
+ */
+ScreenInformation *ScreenInformation::Get(char *which)
+{
+	if (which == nullptr || which[0]=='\0') return nullptr;
+	if (!strcmp(which, name)) return this;
+	if (!next) return nullptr;
+	return next->Get(which);
+}
+
+
+} //namespace Laxkit
+
---
+++ lax/screeninformation.h
@@ -0,0 +1,53 @@
+//
+//	
+//    The Laxkit, a windowing toolkit
+//    Please consult https://github.com/Laidout/laxkit about where to send any
+//    correspondence about this software.
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Library General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Library General Public License for more details.
+//
+//    You should have received a copy of the GNU Library General Public
+//    License along with this library; If not, see <http://www.gnu.org/licenses/>.
+//
+//    Copyright (C) 2004-2013 by Tom Lechner
+//
+#ifndef _LAX_SCREENINFORMATION_H
+#define _LAX_SCREENINFORMATION_H
+
+
+namespace Laxkit {
+
+//--------------------------- ScreenInformation -------------------------------
+class ScreenInformation
+{
+  public:
+	char *name;
+	int primary;
+	int screen; //screen number, can contain many monitors
+	int monitor; //id of monitor within larger virtual screen space
+	int x,y, width,height;
+	int mmwidth,mmheight;
+	int depth;
+
+	ScreenInformation *next;
+
+	ScreenInformation() { next = nullptr; }
+	~ScreenInformation() { delete name; if (next) delete next; }
+	int HowMany() { return 1 + (next ? next->HowMany() : 0); }
+	ScreenInformation *Get(int i) { if (i==0) return this; else { if (next && i>0) return next->Get(i-1); else return nullptr; } }
+	ScreenInformation *Get(char *which);
+};
+
+} //namespace Laxkit
+
+
+#endif
+
