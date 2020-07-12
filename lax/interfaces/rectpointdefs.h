efe7397e7c30d8b560feaddb289bdd3a29a52606
Tom Lechner <tom@tomlechner.com>
2020-07-11 21:07:05 -0700

    Use enum not define

--- lax/interfaces/rectpointdefs.h
+++ lax/interfaces/rectpointdefs.h
@@ -23,73 +23,78 @@
 #define _LAX_RECTPOINTDEFS_H
 
 
-//This file has a bunch of defines that are used by the code in RectInterface and ObjectInterface.
+//This file has a bunch of definitions that are used by RectInterface and ObjectInterface.
 
 
 // These are returned for various draggable points and areas
-#define RP_None        0
-#define RP_Move        1
+enum RectHandles {
+	RP_None = 0,
+	RP_Move,
 
-#define RP_Center1     2
-#define RP_Center2     3
-#define RP_Shearpoint  4
+	RP_Center1,
+	RP_Center2,
+	RP_Shearpoint,
 
-#define RP_Rotate_NE   5
-#define RP_Rotate_NW   6
-#define RP_Rotate_SE   7
-#define RP_Rotate_SW   8
-#define RP_Rotate_Any  9
+	RP_Rotate_NE,
+	RP_Rotate_NW,
+	RP_Rotate_SE,
+	RP_Rotate_SW,
+	RP_Rotate_Any,
+	RP_Rotate_Num,
+	RP_Rotate_Diff,
 
-#define RP_Persp_NE    10
-#define RP_Persp_NW    11
-#define RP_Persp_SE    12
-#define RP_Persp_SW    13
+	RP_Persp_NE,
+	RP_Persp_NW,
+	RP_Persp_SE,
+	RP_Persp_SW,
 
-#define RP_Shear_N     14
-#define RP_Shear_W     15
-#define RP_Shear_S     16
-#define RP_Shear_E     17
+	RP_Shear_N,
+	RP_Shear_W,
+	RP_Shear_S,
+	RP_Shear_E,
 
-#define RP_Scale_N     18
-#define RP_Scale_NW    19
-#define RP_Scale_W     20
-#define RP_Scale_SW    21
-#define RP_Scale_S     22
-#define RP_Scale_SE    23
-#define RP_Scale_E     24
-#define RP_Scale_NE    25
+	RP_Scale_N,
+	RP_Scale_NW,
+	RP_Scale_W,
+	RP_Scale_SW,
+	RP_Scale_S,
+	RP_Scale_SE,
+	RP_Scale_E,
+	RP_Scale_NE,
+	RP_Scale_Num,
 
-#define RP_Flip_H      26
-#define RP_Flip_V      27
-#define RP_Flip1       28
-#define RP_Flip2       29
-#define RP_Flip_Go     30
+	RP_Flip_H,
+	RP_Flip_V,
+	RP_Flip1,
+	RP_Flip2,
+	RP_Flip_Go,
 
-// These are used for convenience when computing handle dragging
-#define RP_SW          101
-#define RP_W           102
-#define RP_NW          103
-#define RP_N           104
-#define RP_NE          105
-#define RP_E           106
-#define RP_SE          107
-#define RP_S           108
-#define RP_Middle      109
+	// These are used for convenience when computing handle dragging
+	RP_SW = 101,
+	RP_W,
+	RP_NW,
+	RP_N,
+	RP_NE,
+	RP_E,
+	RP_SE,
+	RP_S,
+	RP_Middle,
 
-#define RP_MAX         109
-
+	RP_MAX
+};
 
 
 #define HAS_CENTER1     1
 #define HAS_CENTER2     2
 #define HAS_SHEARPOINT  4
 
-// For RectInterface decorations
+// For RectInterface::showdecs
 #define SHOW_OUTER_HANDLES  (1<<0)
 #define SHOW_INNER_HANDLES  (1<<1)
 #define SHOW_TARGET         (1<<2)
 #define SHOW_LINK_BALL      (1<<3)
 #define SHOW_CONSTRAINTS    (1<<4)
+#define SHOW_FLIP_CONTROLS  (1<<5)
 
 
 
